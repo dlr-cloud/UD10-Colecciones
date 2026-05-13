2d007fba7cf1aaf8429515c14e858419496b43fd
dlr-cloud <davidlopezrodriguez7806@gmail.com>
2026-05-13 10:04:41 +0200

    Add files via upload

---
+++ a12.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package a01;
+import java.util.*;
+
+public class a12 {
+
+    public static void main(String[] args) {
+        List<Integer> premios = new ArrayList<>(List.of(1000, 50, 500, 50, 200));
+        int Cantidad = Collections.frequency(premios, 50);
+        System.out.println("Hay un total de: "+ Cantidad + " premios de 50");
+        System.out.println("La cantidad maxima de premios es: " + Collections.max(premios) + " \ny la cantidad minima de premio es: "+ Collections.min(premios));
+        
+        Collections.shuffle(premios);
+        System.out.println("La lista desordenada seria: " + premios);
+        
+        Set<Integer> Orden = new TreeSet<>(premios);
+        System.out.println("La lista ordenada es: " + Orden);
+        
+        int posicion = Collections.binarySearch(premios, 500);
+        System.out.println("La posicion de 500 es: " + posicion);
+    }
+    
+}
